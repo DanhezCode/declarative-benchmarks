fd47207b95db2ef8cee2b73d28a0e14159f48faf
DanhezCode <danhezcode@gmail.com>
2026-01-09 04:38:29 -0500

    fix: update CI configuration and enhance semantic release setup

--- release.config.js
+++ release.config.js
@@ -1,14 +1,24 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
+/** @type {import('semantic-release').GlobalConfig} */
 export default {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    ["@semantic-release/npm", { npmPublish: true }],
-    ["@semantic-release/git", { assets: ["CHANGELOG.md"] }],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        pkgRoot: "./",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
   ],
 };
