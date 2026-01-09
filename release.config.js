9b25f4c16ba081d50a976f758eb0027a6cd95061
DanhezCode <danhezcode@gmail.com>
2026-01-09 06:32:00 -0500

    feat: restructure CI/CD with new configurations and add support for development branches

--- release.config.js
+++ release.config.js
@@ -1,22 +1,30 @@
 /** @type {import('semantic-release').GlobalConfig} */
 export default {
-  branches: ["main"],
+  branches: ["main", { name: "develop", prerelease: "beta", channel: "beta" }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle:
+          "# Changelog\n\nAll notable changes to this project will be documented in this file.",
+      },
+    ],
     [
       "@semantic-release/npm",
       {
         npmPublish: true,
         pkgRoot: "./",
+        tarballDir: "release",
       },
     ],
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json"],
-        message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
     "@semantic-release/github",
